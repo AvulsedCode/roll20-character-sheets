300135d5c69a1ad07863e7f76f9d370dcca0882f
AvulsedCode <gamemaster@avulsedgames.com>
2024-09-16 19:50:36 -0500

    added a bunc more samples

---
+++ MythCraft/Compendium/Spellbook/DivineSource(pg231).js
@@ -0,0 +1,103 @@
+const Divine  = {
+    DescriptiveParagraphs: [
+        "",
+        "",
+        ""],
+    Focus: "",
+    IntentParagraphs: [
+        "",
+        ""
+    ],
+    BacklashParagraphs:
+        [
+            "",
+            "",
+            ""
+        ],
+    CriticalCasts: "",
+    MagicTypes: {
+        additional: "",
+        cantrip: { hasCantrip: true, cantripText: "" }
+    },
+    ArcaneTags: {
+        descriptionTest: "",
+        tagsList: [
+            { tagName: "", TagDescription: "" },
+        ]
+    },
+    TalentStacks: [
+      
+        
+        {
+            displayName: "",
+            StackName: "",
+            prerequisites: [""],
+            isRepeatable: true,
+            incompatableWith: [""],
+            flavortext: "",
+            BenefitList: [  
+                { benefitName: "", BenefitDescription: "" },
+            ],
+        }
+
+
+    ],
+    explinationText: {
+        headding: "",
+        description: ""
+    },
+    Cantrips: [
+         {
+            cantripName: "",
+            prerequisites: [""],
+            APC: 2,
+            Range: 0,
+            Requires: "Gesture, Line of Effect",
+            Duration: "Instantaneous",
+            cantripDescription: "",
+            itallics: "",
+            scaling: [
+                { level: "", inproveRange: 0, improveDamage: "", addEffect: "" },
+           ]
+        }
+    ],
+Prayers:[{
+    PrayerName: "",
+    prerequisites: [""],
+    APC: 2,
+    Range: 0,
+    Requires: "Gesture, Line of Effect",
+    Duration: "Instantaneous",
+    prayerDescription: "",
+    itallics: "",
+    scaling: [
+        { level: "", inproveRange: 0, improveDamage: "", addEffect: "" },
+   ]
+}
+],
+Rituals:[{
+    ritualName:"",
+    prerequisites: [""],
+    APC: 2,
+    SPC: 2,
+    Range: "",
+    Requires: "",
+    Duration: "",
+    ritualDescriptionParagraphs:[""],
+    Upcharges:[ ""],
+},
+],
+Spells:[
+{
+    spellName:"",
+    prerequisites: [""],
+    APC: 2,
+    SPC: 2,
+    Range: "",
+    Requires: "",
+    Duration: "",
+    spellDescriptionParagraphs:[""],
+    Upcharges:[ ""],
+},      
+]
+};
